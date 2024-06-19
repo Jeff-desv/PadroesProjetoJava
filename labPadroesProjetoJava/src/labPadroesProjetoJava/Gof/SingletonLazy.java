586a55281419df34249db12f6cee78f10b6b47c2
Jeff-desv <brunojeff0012@gmail.com>
2024-06-19 20:35:01 -0300

    Iniciando Projeto

---
+++ labPadroesProjetoJava/src/labPadroesProjetoJava/Gof/SingletonLazy.java
@@ -0,0 +1,23 @@
+package labPadroesProjetoJava.Gof;
+
+
+/**
+ * Singleton
+ * @author Jeff-desv
+ */
+public class SingletonLazy {
+	
+	private static SingletonLazy instancia;
+	
+	private SingletonLazy() {
+		super();
+	}
+	
+	public static SingletonLazy getInstancia() {
+		if(instancia == null) {
+			instancia = new SingletonLazy();
+		}
+		return instancia;
+	}
+	
+}

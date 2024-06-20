e32b5e35fbc97e8c3dc733889a0eeac91e51343b
Jeff-desv <brunojeff0012@gmail.com>
2024-06-20 17:53:57 -0300

    Criando a Classe Teste

---
+++ labPadroesProjetoJava/src/labPadroesProjetoJava/Gof/TesteSingleton.java
@@ -0,0 +1,11 @@
+package labPadroesProjetoJava.Gof;
+
+public class TesteSingleton {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SingletonLazy lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+	}
+
+}

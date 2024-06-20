1e52787691c98ad0307a834d7d0100713a550d74
Jeff-desv <brunojeff0012@gmail.com>
2024-06-20 17:57:20 -0300

    Testes Padroes de Projeto Java

--- labPadroesProjetoJava/src/labPadroesProjetoJava/Gof/TesteSingleton.java
+++ labPadroesProjetoJava/src/labPadroesProjetoJava/Gof/TesteSingleton.java
@@ -5,12 +5,15 @@ public class TesteSingleton {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SingletonLazy lazy = SingletonLazy.getInstancia();
+		System.out.println();
 		System.out.println(lazy);
 		
 		SingletonEager eager = SingletonEager.getInstancia();
+		System.out.println();
 		System.out.println(eager);
 		
 		SingletonLazyHolder holder = SingletonLazyHolder.getInstancia();
+		System.out.println();
 		System.out.println(holder);
 	}
 
